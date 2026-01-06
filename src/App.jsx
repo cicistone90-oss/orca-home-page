99974fb423a60e50a90be97fbc1e890b41029afb
Yuqin Shi <shiyuqin@dfmc.com.cn>
2026-01-06 15:37:43 +0800

    Fix GitHub Pages deployment - add SPA routing support and use HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage.jsx';
 import InfoPage from './pages/InfoPage.jsx';
 import PrivacyPolicy from './pages/PrivacyPolicy.jsx';
